20228881cbce6bf7276356556f41c41a131f5a00
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-23 12:49:22 -0300

    [I-01] created migrations for the first structure of database

---
+++ database/migrations/2020_05_23_151805_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->unsignedBigInteger('wallet_id');
+            $table->unsignedBigInteger('comment_id')->nullable();
+            $table->uuid('parent_id')->nullable();
+            $table->enum('type', ['in', 'out']);
+            $table->float('amount');
+            $table->timestamps();
+
+
+            $table->foreign('wallet_id')->references('id')->on('wallets');
+            $table->foreign('comment_id')->references('id')->on('comments');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
