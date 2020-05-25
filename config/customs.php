52ac6a27eccf0c0bfb281571c5f157e551b2d29a
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-25 13:49:14 -0300

    [I-01] added custom file for constant custom values used in the API

---
+++ config/customs.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom API Properties
+    |--------------------------------------------------------------------------
+    |
+    | Array to define the many custom values and properties of the API.
+    |
+    */
+
+    'transaction' => [
+        'percentage' => 0.1,
+    ],
+
+    'comments' => [
+        'limitPerMinute' => 5,
+    ]
+];
