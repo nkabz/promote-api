b49e5219e7419bf551f5bbd007936d20fa983c31
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-25 19:03:42 -0300

    [I-01] creating Auth function to login via header (temporary until sanctum implementation

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next)
+    {
+        Auth::loginUsingId($request->header('clientId'));
+
+        return $next($request);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
