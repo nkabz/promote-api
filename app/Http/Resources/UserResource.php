e1dcfb3aea000ac426da0664b3d7882559860a35
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-26 21:44:00 -0300

    [I-01] new user resource for returning user in comments

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CommentResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,10 +16,8 @@ class CommentResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'user' => new UserResource($this->user),
-            'isHighlighted' => $this->is_highlighted,
-            'createdAt' => $this->created_at,
-            'content' => $this->content,
+            'login' => $this->email,
+            'userIsSubscriber' => (bool) $this->subscriber,
         ];
     }
 }
