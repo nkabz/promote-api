4a337bb1336282585970e4f9c352a15919725d6c
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-28 02:13:03 -0300

    [I-02] Remove previous authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,23 +9,6 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 class Authenticate extends Middleware
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string[]  ...$guards
-     * @return mixed
-     *
-     * @throws \Illuminate\Auth\AuthenticationException
-     */
-    public function handle($request, Closure $next)
-    {
-        Auth::loginUsingId($request->header('clientId'));
-
-        return $next($request);
-    }
-
-    /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
