43a6f9b5d9170e6f05799868c7f1fe6c47e5b053
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-28 02:12:45 -0300

    [I-02] Sanctum base files and trait added

--- app/User.php
+++ app/User.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Carbon\Carbon;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     protected static function booted()
     {
